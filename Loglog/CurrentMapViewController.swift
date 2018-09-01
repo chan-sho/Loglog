8e997fd4b7c61faad1d64360fd30d5355262b30b
高野翔 <ShoTakano@43x232x203x168.ap43.ftth.ucom.ne.jp>
2018-09-02 03:56:55 +0900

    途中経過２

--- Loglog/CurrentMapViewController.swift
+++ Loglog/CurrentMapViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class CurrentMapViewController: UIViewController {
-
+class CurrentMapViewController: UIViewController, CLLocationManagerDelegate  {
+    
+    @IBOutlet weak var currentMapView: MKMapView!
+    
+    var currentMapManager:CLLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //デリゲート先に自分を設定する。
+        currentMapManager.delegate = self
+        //位置情報の取得を開始する。
+        currentMapManager.startUpdatingLocation()
+        //位置情報の利用許可を変更する画面をポップアップ表示する。
+        currentMapManager.requestWhenInUseAuthorization()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +34,98 @@ class CurrentMapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //画面表示後の呼び出しメソッド
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if(CLLocationManager.locationServicesEnabled() == true){
+            switch CLLocationManager.authorizationStatus() {
+                
+            //未設定の場合
+            case CLAuthorizationStatus.notDetermined:
+                currentMapManager.requestWhenInUseAuthorization()
+                
+            //機能制限されている場合
+            case CLAuthorizationStatus.restricted:
+                alertMessage(message: "位置情報サービスの利用が制限されている利用できません。「設定」⇒「一般」⇒「機能制限」")
+                
+            //「許可しない」に設定されている場合
+            case CLAuthorizationStatus.denied:
+                alertMessage(message: "位置情報の利用が許可されていないため利用できません。「設定」⇒「プライバシー」⇒「位置情報サービス」⇒「アプリ名」")
+                
+            //「このAppの使用中のみ許可」に設定されている場合
+            case CLAuthorizationStatus.authorizedWhenInUse:
+                //位置情報の取得を開始する。
+                currentMapManager.startUpdatingLocation()
+                
+            //「常に許可」に設定されている場合
+            case CLAuthorizationStatus.authorizedAlways:
+                //位置情報の取得を開始する。
+                currentMapManager.startUpdatingLocation()
+            }
+            
+        } else {
+            //位置情報サービスがOFFの場合
+            alertMessage(message: "位置情報サービスがONになっていないため利用できません。「設定」⇒「プライバシー」⇒「位置情報サービス」")
+        }
     }
-    */
-
+    
+    //メッセージ出力メソッド
+    func alertMessage(message:String) {
+        let aleartController = UIAlertController(title: "注意", message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title:"OK", style: .default, handler:nil)
+        aleartController.addAction(defaultAction)
+        
+        present(aleartController, animated:true, completion:nil)
+        
+    }
+    
+    //位置情報取得時の呼び出しメソッド
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        for location in locations {
+            
+            //中心座標
+            let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+            
+            //表示範囲
+            let span = MKCoordinateSpanMake(0.01, 0.01)
+            
+            //中心座標と表示範囲をマップに登録する。
+            let region = MKCoordinateRegionMake(center, span)
+            currentMapView.setRegion(region, animated:true)
+            
+            //ピンを作成してマップビューに登録する。
+            let pin = MKPointAnnotation()
+            pin.coordinate = CLLocationCoordinate2DMake(location.coordinate.latitude, pin.coordinate.longitude)
+            self.currentMapView.addAnnotation(pin)
+            //ピンのタイトルを設定
+            pin.title = "この場所で良いですか？"
+            //ピンのサブタイトルの設定
+            pin.subtitle = "OKなら「投稿」をクリック"
+            
+        }
+    }
+    
+    @IBAction func longPressGesture(_ sender: UILongPressGestureRecognizer) {
+        //この処理を書くことにより、指を離したときだけ反応するようにする（何回も呼び出されないようになる。最後の話したタイミングで呼ばれる）
+        if sender.state != UIGestureRecognizerState.began {
+            return
+        }
+        
+        //senderから長押しした地図上の座標を取得
+        let tappedLocation = sender.location(in: currentMapView)
+        let tappedPoint = currentMapView.convert(tappedLocation, toCoordinateFrom: currentMapView)
+        // update annotation
+        currentMapView.removeAnnotations(currentMapView.annotations)
+        //ピンの生成
+        let pin = MKPointAnnotation()
+        //ピンを置く場所を指定
+        pin.coordinate = tappedPoint
+        //ピンのタイトルを設定
+        pin.title = "この場所で良いですか？"
+        //ピンのサブタイトルの設定
+        pin.subtitle = "OKなら「投稿」をクリック"
+        //ピンをdisplayMapの上に置く
+        self.currentMapView.addAnnotation(pin)
+    }
 }
--- Loglog/SearchMapViewController.swift
+++ Loglog/SearchMapViewController.swift
@@ -7,29 +7,92 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class SearchMapViewController: UIViewController {
-
+class SearchMapViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var displayMap: MKMapView!
+    
+    var pinView:MKPinAnnotationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        inputText.delegate = self
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        let searchKeyword = textField.text
+        
+        print(searchKeyword!)
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(searchKeyword!, completionHandler: { (placemarks:[CLPlacemark]?, error:Error?) in
+            
+            if let placemark = placemarks?[0] {
+                if let targetCoordinate = placemark.location?.coordinate {
+                    print(targetCoordinate)
+                    
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = targetCoordinate
+                    pin.title = searchKeyword
+                    pin.subtitle = "OKなら「投稿」をクリック"
+                    
+                    self.displayMap.addAnnotation(pin)
+                    self.displayMap.region = MKCoordinateRegionMakeWithDistance(targetCoordinate,500.0,500.0)
+                }
+            }
+        })
+        
+        return true
     }
-    */
+    
+    @IBAction func longPressGesture(_ sender: UILongPressGestureRecognizer) {
+        //この処理を書くことにより、指を離したときだけ反応するようにする（何回も呼び出されないようになる。最後の話したタイミングで呼ばれる）
+        if sender.state != UIGestureRecognizerState.began {
+            return
+        }
+        
+        //senderから長押しした地図上の座標を取得
+        let tappedLocation = sender.location(in: displayMap)
+        let tappedPoint = displayMap.convert(tappedLocation, toCoordinateFrom: displayMap)
+        // update annotation
+        displayMap.removeAnnotations(displayMap.annotations)
+        //ピンの生成
+        let pin = MKPointAnnotation()
+        //ピンを置く場所を指定
+        pin.coordinate = tappedPoint
+        //ピンのタイトルを設定
+        pin.title = "この場所で良いですか？"
+        //ピンのサブタイトルの設定
+        pin.subtitle = "OKなら「投稿」をクリック"
+        //ピンをdisplayMapの上に置く
+        self.displayMap.addAnnotation(pin)
+    }
+    //アノテーションビューを返すメソッド
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        //アノテーションビューを作成する。
+        let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        //アノテーションビューに座標、タイトル、サブタイトルを設定する。
+        pinView.annotation = annotation
+        //アノテーションビューに色を設定する。
+        pinView.backgroundColor = UIColor.white
+        //吹き出しを表示可能にする。
+        pinView.canShowCallout = true
+        
+        return pinView
+    }
+
 
 }
