f151bd3f5153aff7c5f982cc6137b1aaee4db9b9
高野翔 <ShoTakano@43x232x203x168.ap43.ftth.ucom.ne.jp>
2018-09-02 02:50:13 +0900

    途中経過

--- Loglog/SettingViewController.swift
+++ Loglog/SettingViewController.swift
@@ -7,8 +7,99 @@
 //
 
 import UIKit
+import ESTabBarController
+import Firebase
+import FirebaseAuth
+import SVProgressHUD
 
 class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var displayNameTextField: UITextField!
+    @IBOutlet weak var mailAddressTextField: UITextField!
+    @IBOutlet weak var passWordTextField: UITextField!
+    
+    @IBAction func handleNameChangeButton(_ sender: Any) {
+        if let displayName = displayNameTextField.text {
+            
+            // 表示名が入力されていない時はHUDを出して何もしない
+            if displayName.isEmpty {
+                SVProgressHUD.showError(withStatus: "修正後のアカウント名（表示名）を入力して下さい")
+                return
+            }
+            
+            // 表示名を設定する
+            let user = Auth.auth().currentUser
+            if let user = user {
+                let changeRequest = user.createProfileChangeRequest()
+                changeRequest.displayName = displayName
+                changeRequest.commitChanges { error in
+                    if let error = error {
+                        SVProgressHUD.showError(withStatus: "アカウント名（表示名）の変更に失敗しました。")
+                        print("DEBUG_PRINT: " + error.localizedDescription)
+                        return
+                    }
+                    print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました。")
+                    
+                    // HUDで完了を知らせる
+                    SVProgressHUD.showSuccess(withStatus: "アカウント名（表示名）を変更しました")
+                }
+            }
+        }
+        // キーボードを閉じる
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func handleMailChangeButton(_ sender: Any) {
+        if let mailAddress = mailAddressTextField.text {
+            
+            // 表示名が入力されていない時はHUDを出して何もしない
+            if mailAddress.isEmpty {
+                SVProgressHUD.showError(withStatus: "修正後のメールアドレスを入力して下さい")
+                return
+            }
+            
+                Auth.auth().currentUser?.updateEmail(to: self.mailAddressTextField.text!) { error in
+                    if let error = error {
+                        SVProgressHUD.showError(withStatus: "メールアドレスの変更に失敗しました。")
+                        print("DEBUG_PRINT: " + error.localizedDescription)
+                        return
+                    }
+                    print("DEBUG_PRINT: [emailAddress = \(self.mailAddressTextField.text!)]の設定に成功しました。")
+                    
+                    // HUDで完了を知らせる
+                    SVProgressHUD.showSuccess(withStatus: "メールアドレスを変更しました")
+                }
+            }
+        // キーボードを閉じる
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func handlePasswordChangeButton(_ sender: Any) {
+    }
+    
+    @IBAction func handleLogoutButton(_ sender: Any) {
+        // ログアウトする
+        try! Auth.auth().signOut()
+        
+        // ログイン画面を表示する
+        let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+        self.present(loginViewController!, animated: true, completion: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // ユーザー情報を取得して各TextFieldに設定する（パスワード以外）
+        let user = Auth.auth().currentUser
+        if let user = user {
+            displayNameTextField.text = user.displayName
+        }
+        let userAddress = Auth.auth().currentUser!.email
+        if let userAddress = userAddress {
+            mailAddressTextField.text = userAddress
+        }
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
